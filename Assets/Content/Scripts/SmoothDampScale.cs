0dfcfd7da4ca8522096b025caffa2174e143d79d
Free Debreuil <freedebreuil@gmail.com>
2018-04-14 12:00:31 +0100

    adding smooth damp

---
+++ Assets/Content/Scripts/SmoothDampScale.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothDampScale : MonoBehaviour {
+	public float smoothTime;
+	public Vector3 target;
+	public Vector3 velocity;
+
+	public void Update () {
+		transform.localScale = Vector3.SmoothDamp(transform.localScale, target, ref velocity, smoothTime);
+	}
+}
