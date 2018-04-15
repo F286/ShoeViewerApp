35d8a1e812cf11eae3a1a34b1f933a4f8e7f02fe
Free Debreuil <freedebreuil@gmail.com>
2018-04-15 23:04:56 +0100

    working on orientation

---
+++ Assets/Content/Scene Shoe/Scripts/Setup.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Setup : MonoBehaviour {
+
+	public void Awake() {
+		Screen.orientation = ScreenOrientation.LandscapeLeft;
+	}
+}
