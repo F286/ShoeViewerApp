b12e6eae9f5ca9f232fa2cf2e6a2f114dac1750b
Free Debreuil <freedebreuil@gmail.com>
2018-04-15 21:42:23 +0100

    coloring of shoe

--- Assets/Content/Scene Shoe/Scripts/Shoe.cs
+++ Assets/Content/Scene Shoe/Scripts/Shoe.cs
@@ -4,8 +4,27 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class Shoe : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler {
+  public Color primary = Color.white;
+  public Color secondary = Color.white;
+  public Color tertiary = Color.white;
+  [Space()]
   public Transform graphic;
   public SmoothDampVector3 scale;
+  public MeshRenderer[] primaryGraphic;
+  public MeshRenderer[] secondaryGraphic;
+  public MeshRenderer[] tertiaryGraphic;
+
+  public void Update() {
+    foreach (var item in primaryGraphic) {
+      item.material.color = primary;
+    }
+    foreach (var item in secondaryGraphic) {
+      item.material.color = secondary;
+    }
+    foreach (var item in tertiaryGraphic) {
+      item.material.color = tertiary;
+    }
+  }
   
   public void LateUpdate() {
     graphic.localScale = scale.current;
