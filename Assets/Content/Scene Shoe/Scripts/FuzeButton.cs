ef71ceab6f8362aac4d8334f980285be41c01436
Free Debreuil <freedebreuil@gmail.com>
2018-04-18 00:10:23 +0100

    added fuze button logic

---
+++ Assets/Content/Scene Shoe/Scripts/FuzeButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class FuzeButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+	public bool isEnter;
+	public float enterTime;
+
+	const float triggerTime = 1f;
+	
+  public void OnPointerEnter(PointerEventData eventData) {
+		isEnter = true;
+		enterTime = Time.time;
+  }
+
+  public void OnPointerExit(PointerEventData eventData) {
+		isEnter = false;
+  }
+
+	public void Update() {
+		if (isEnter) {
+			if (Time.time - enterTime > triggerTime) {
+				isEnter = false;
+				GetComponent<Button>().onClick.Invoke();
+			}
+		}
+	}
+
+}
