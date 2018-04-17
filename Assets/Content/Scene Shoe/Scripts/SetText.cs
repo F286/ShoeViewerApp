dc782cb0ff7867680955ef2f77964d0466c28e4e
Free Debreuil <freedebreuil@gmail.com>
2018-04-17 22:14:32 +0100

    set text

---
+++ Assets/Content/Scene Shoe/Scripts/SetText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetText : MonoBehaviour {
+	public Type type;
+	public int offset;
+
+	public void Update () {
+		var data = ShoeDataManager.instance.GetData(offset);
+
+		switch (type) {
+			case Type.Title:
+				GetComponent<Text>().text = data.title;
+				break;
+			case Type.Price:
+				GetComponent<Text>().text = data.price;
+				break;
+		}
+	}
+
+	public enum Type {
+		Title,
+		Price
+	}
+}
