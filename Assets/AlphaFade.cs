3d82e69b21a733ec2f351a309ffdff2e0d6fcff6
Free Debreuil <freedebreuil@gmail.com>
2018-04-19 22:42:49 +0100

    fading

---
+++ Assets/AlphaFade.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlphaFade : MonoBehaviour {
+	public CanvasGroup group;
+	public SmoothDampFloat smooth;
+
+	bool wasShow = false;
+
+	public void FadeIn() {
+		smooth.target = 1;
+	}
+	public void FadeOut() {
+		smooth.target = 0;
+	}
+
+	// public void OnEnable() {
+	// 	wasShow = smooth.current > 0.001f;
+	// 	group.gameObject.SetActive(wasShow);
+	// }
+	public void Update () {
+		group.alpha = smooth.current;
+
+		var isShow = smooth.current > 0.001f;
+		if (isShow != group.gameObject.activeSelf) {
+			group.gameObject.SetActive(isShow);
+		}
+		// if (isShow != wasShow) {
+		// 	group.gameObject.SetActive(isShow);
+		// }
+	}
+}
---
+++ Assets/Content/Scene Shoe/Scripts/SmoothDampFloat.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothDampFloat : MonoBehaviour {
+	public float smoothTime = 0.2f;
+	[Range(0, 1)]
+	public float current = 0;
+	[Range(0, 1)]
+	public float target = 0;
+	[Range(0, 1)]
+	public float velocity = 0;
+
+	public void Update () {
+		current = Mathf.SmoothDamp(current, target, ref velocity, smoothTime);
+	}
+}
