b6f190f2595eaa75975b4ee62f3e0bf2f35abfb7
Free Debreuil <freedebreuil@gmail.com>
2018-04-21 10:13:50 +0100

    working on shoe animate

---
+++ Assets/ShoeAnimate.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShoeAnimate : MonoBehaviour {
+	// public SmoothDampFloat offset;
+	public Transform[] shoes;
+	// public Transform[] targets;
+
+	public void Next() {
+		GameObject.FindWithTag("Setup").GetComponent<Setup>().StartCoroutine(AnimateTo(1));
+	}
+	public void Previous() {
+		GameObject.FindWithTag("Setup").GetComponent<Setup>().StartCoroutine(AnimateTo(-1));
+	}
+	public IEnumerator AnimateTo(int offset) {
+		var t = 0f;
+
+		while(t < 1) {
+			t += Time.smoothDeltaTime;
+
+			foreach (var item in shoes) {
+				item.localScale = Vector3.Lerp(Vector3.one, Vector3.zero, t);
+			}
+			yield return new WaitForEndOfFrame();
+		}
+		
+		t = 0f;
+		while(t < 1) {
+			t += Time.smoothDeltaTime;
+
+			foreach (var item in shoes) {
+				item.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, t);
+			}
+			yield return new WaitForEndOfFrame();
+		}
+	}
+}
