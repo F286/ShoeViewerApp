672a0f7269afc1ff7c785e53e4f22ecd46cf62c5
Free Debreuil <freedebreuil@gmail.com>
2018-04-21 11:48:15 +0100

    ambient

--- Assets/Content/Scene Shoe/Scripts/AmbientRotation.cs
+++ Assets/Content/Scene Shoe/Scripts/AmbientRotation.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AmbientRotation : MonoBehaviour {
-	public Vector3 velocity;
+	public Vector3 velocity = new Vector3(20, 0, 0);
 
 	public void Update () {
 		transform.localRotation *= Quaternion.Euler(velocity * Time.smoothDeltaTime);
